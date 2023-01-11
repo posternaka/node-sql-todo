1b50eb2c243ac43b778797e1d5458ec27fc54cf8
posternaka <nik.petrenko97@gmail.com>
2023-01-12 02:03:45 +0300

    adding data to the database and displaying it on the page

--- routes/todo.js
+++ routes/todo.js
@@ -1,24 +1,58 @@
-const {Router} = require('express')
-const router = Router()
+const {Router} = require('express');
+const Todo = require('../models/todo');
+const router = Router();
 
 // Получение списка задач
-router.get('/', (req, res) => {
-  res.json({a: 1})
-})
+router.get('/', async (req, res) => {
+  try {
+    const todos = await Todo.findAll();
+    res.status(200).json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Server error'
+    });
+  }
+});
 
 // Создание новой задачи
-router.post('/', (req, res) => {
-
-})
+router.post('/', async (req, res) => {
+  try {
+    const todo = await Todo.create({
+      title: req.body.title,
+      done: false
+    });
+    res.status(201).json({todo});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Server error'
+    });
+  }
+});
 
 // Изменение задачи
 router.put('/:id', (req, res) => {
-
-})
+  try {
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Server error'
+    });
+  }
+});
 
 // Удаление задачи
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Server error'
+    });
+  }
+});
 
-})
-
-module.exports = router
+module.exports = router;
